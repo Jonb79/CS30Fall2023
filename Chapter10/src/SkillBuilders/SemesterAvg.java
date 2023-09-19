4fd96a72009b1779140c35d8d48f5d530476cdc8
Jonb79 <123979513+Jonb79@users.noreply.github.com>
2023-09-19 15:20:30 -0600

    Add files via upload

---
+++ Chapter10/src/SkillBuilders/SemesterAvg.java
@@ -0,0 +1,54 @@
+package SkillBuilders;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Font;
+
+public class SemesterAvg 
+{
+
+	private JFrame frmSemesterAverage;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) 
+	{
+		EventQueue.invokeLater(new Runnable() 
+		{
+			public void run() 
+			{
+				try 
+				{
+					SemesterAvg window = new SemesterAvg();
+					window.frmSemesterAverage.setVisible(true);
+				} catch (Exception e) 
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public SemesterAvg() 
+	{
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() 
+	{
+		frmSemesterAverage = new JFrame("Semester Average");
+		frmSemesterAverage.setOpacity(2.0f);
+		frmSemesterAverage.setFont(new Font("Dialog", Font.BOLD, 13));
+		frmSemesterAverage.setBounds(100, 100, 450, 300);
+		frmSemesterAverage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
