1b6c412ed2b65008d21b399319aa6ab4385b7bb9
Jonb79 <123979513+Jonb79@users.noreply.github.com>
2023-09-19 14:42:58 -0600

    Add files via upload

--- Chapter10/src/DemoSep14.java
+++ Chapter10/src/DemoSep14.java
@@ -47,12 +47,12 @@ public class DemoSep14 {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 492, 476);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 434, 10);
+		panel.setBounds(0, 0, 375, 1);
 		frame.getContentPane().add(panel);
 		
 		fn = new JTextField();
@@ -68,8 +68,20 @@ public class DemoSep14 {
 		ln.setColumns(10);
 		
 		JTextArea disp = new JTextArea();
+		disp.setFont(new Font("Monospaced", Font.PLAIN, 15));
 		disp.setBounds(10, 121, 317, 107);
 		frame.getContentPane().add(disp);
+		
+		JComboBox CB1 = new JComboBox();
+		CB1.setModel(new DefaultComboBoxModel(new String[] {"Select Grade", "10", "11", "12"}));
+		CB1.setBounds(10, 62, 147, 22);
+		frame.getContentPane().add(CB1);
+		
+		JComboBox CB2 = new JComboBox();
+		CB2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		CB2.setModel(new DefaultComboBoxModel(new String[] {"Select School", "Crescent", "Western", "Eastern"}));
+		CB2.setBounds(180, 62, 135, 22);
+		frame.getContentPane().add(CB2);
 		
 		JButton btnNewButton = new JButton("Submit");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -77,11 +89,40 @@ public class DemoSep14 {
 			{
 				String fN = fn.getText();
 				String lN = ln.getText();
+				String grade = "";
+				String school = "";
+				
+				if(CB1.getSelectedItem().equals("10"))
+				{
+					grade = "10";
+				}
+				else if(CB1.getSelectedItem().equals("11"))
+				{
+					grade = "11";
+				}
+				else 
+				{
+					grade = "12";
+				}
+				
+				if(CB2.getSelectedItem().equals("Crescent"))
+				{
+					school = "Crescent";
+				}
+				else if(CB2.getSelectedItem().equals("Western"))
+				{
+					school = "Western";
+				}
+				else 
+				{
+					school = "Eastern";
+				}
+				
 				
 				disp.setText
 				(
 					fN + " " +	
-					lN 	
+					lN + " is in grade: " +	grade + " and goes to: " + school + " high school"
 						
 				);
 			}
@@ -89,19 +130,10 @@ public class DemoSep14 {
 			
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnNewButton.setBounds(337, 25, 89, 119);
+		btnNewButton.setBounds(337, 21, 89, 207);
 		frame.getContentPane().add(btnNewButton);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Select Grade", "10", "11", "12"}));
-		comboBox.setBounds(10, 62, 147, 22);
-		frame.getContentPane().add(comboBox);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Select School", "Crescent", "Western", "Eastern"}));
-		comboBox_1.setBounds(180, 62, 135, 22);
-		frame.getContentPane().add(comboBox_1);
 		
 		
 	}
