e77572ac11de0911a058b51875fd170a68561149
26031001 <26031001@L815WD220011.edu.cbe.ab.ca>
2023-09-25 13:55:15 -0600

    Committed at 1:55

--- Chapter10/src/DemoSep14.java
+++ Chapter10/src/DemoSep14.java
@@ -10,10 +10,13 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
+
 import java.awt.Font;
 import javax.swing.JTextArea;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
 
 public class DemoSep14 {
 
@@ -45,14 +48,21 @@ public class DemoSep14 {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() 
+	{
+		ImageIcon cres = new ImageIcon("C:\\Users\\26031001\\git\\CS30Fall2023\\Chapter10\\cres.png");
+		ImageIcon west = new ImageIcon("C:\\Users\\26031001\\git\\CS30Fall2023\\Chapter10\\west.png");
+		ImageIcon east = new ImageIcon("C:\\Users\\26031001\\git\\CS30Fall2023\\Chapter10\\east.jpg");
+		
+		
+		
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 492, 476);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(0, 0, 434, 10);
+		panel.setBounds(0, 0, 375, 1);
 		frame.getContentPane().add(panel);
 		
 		fn = new JTextField();
@@ -68,8 +78,24 @@ public class DemoSep14 {
 		ln.setColumns(10);
 		
 		JTextArea disp = new JTextArea();
-		disp.setBounds(10, 121, 317, 107);
+		disp.setFont(new Font("Monospaced", Font.PLAIN, 13));
+		disp.setBounds(10, 121, 456, 107);
 		frame.getContentPane().add(disp);
+		
+		JComboBox CB1 = new JComboBox();
+		CB1.setModel(new DefaultComboBoxModel(new String[] {"Select Grade", "10", "11", "12"}));
+		CB1.setBounds(10, 62, 147, 22);
+		frame.getContentPane().add(CB1);
+		
+		JComboBox CB2 = new JComboBox();
+		CB2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		CB2.setModel(new DefaultComboBoxModel(new String[] {"Select School", "Crescent", "Western", "Eastern"}));
+		CB2.setBounds(180, 62, 135, 22);
+		frame.getContentPane().add(CB2);
+		
+		JLabel di = new JLabel("");
+		di.setBounds(32, 259, 369, 167);
+		frame.getContentPane().add(di);
 		
 		JButton btnNewButton = new JButton("Submit");
 		btnNewButton.addActionListener(new ActionListener() {
@@ -77,11 +103,48 @@ public class DemoSep14 {
 			{
 				String fN = fn.getText();
 				String lN = ln.getText();
+				String grade = "";
+				String school = "";
+				
+				if(CB1.getSelectedItem().equals("10"))
+				{
+					grade = "10";
+				}
+				else if(CB1.getSelectedItem().equals("11"))
+				{
+					grade = "11";
+				}
+				else 
+				{
+					grade = "12";
+				}
+				
+				if(CB2.getSelectedItem().equals("Crescent"))
+				{
+					school = "Crescent";
+					
+					di.setIcon(cres);
+					
+					
+				}
+				else if(CB2.getSelectedItem().equals("Western"))
+				{
+					school = "Western";
+					
+					di.setIcon(west);
+				}
+				else 
+				{
+					school = "Eastern";
+					
+					di.setIcon(east);
+				}
+				
 				
 				disp.setText
 				(
 					fN + " " +	
-					lN 	
+					lN + " is in grade: " +	grade + " and goes to: " + school + " high school"
 						
 				);
 			}
@@ -89,19 +152,11 @@ public class DemoSep14 {
 			
 		});
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 15));
-		btnNewButton.setBounds(337, 25, 89, 119);
+		btnNewButton.setBounds(377, 21, 89, 93);
 		frame.getContentPane().add(btnNewButton);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Select Grade", "10", "11", "12"}));
-		comboBox.setBounds(10, 62, 147, 22);
-		frame.getContentPane().add(comboBox);
+
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Select School", "Crescent", "Western", "Eastern"}));
-		comboBox_1.setBounds(180, 62, 135, 22);
-		frame.getContentPane().add(comboBox_1);
 		
 		
 	}
